9c226bcf6e647781d825b513d473be144c29fef8
Masahiro Honma <hiratara@cpan.org>
2013-04-09 00:10:12 +0900

    Implement basic concepts.

--- lib/Log/Fibre.pm
+++ lib/Log/Fibre.pm
@@ -5,6 +5,54 @@ use warnings;
 
 our $VERSION = '0.01';
 
+sub new {
+    my ($class, $logger) = @_;
+    bless {logger => $logger, fibres => {}} => $class;
+}
+
+sub fibre {
+    my $self = shift;
+    my $meth = shift;
+    my $option = pop if ref $_[-1] eq 'HASH';
+
+    my $time = time;
+
+    my (undef, $file, $line) = caller;
+    my $fibre_name = join "\t" => $file, $line;
+
+    my $fibre = $self->{fibres}{$fibre_name} //= {
+        start_time => $time, count => 0,
+        last_method => undef, last_log => undef,
+    };
+    $fibre->{count}++;
+
+    if ($option->{max} && $fibre->{count} >= $option->{max}) {
+        $self->{logger}->$meth(@_);
+        delete $self->{fibres}{$fibre_name};
+    } elsif ($option->{duration} &&
+             $time - $fibre->{start_time} >= $option->{duration}) {
+        $self->{logger}->$meth(@_);
+        delete $self->{fibres}{$fibre_name};
+    } else {
+        $fibre->{last_method} = $meth;
+        $fibre->{last_log} = [@_];
+    }
+}
+
+sub DESTROY {
+    my $self = shift;
+    for (keys %{$self->{fibres}}) {
+        my $fibre = $self->{fibres}{$_};
+        my $meth = $fibre->{last_method};
+        $self->{logger}->$meth(@{$fibre->{last_log}});
+    }
+}
+
+sub AUTOLOAD {
+    my $self = shift;
+    (my $method = our $AUTOLOAD) =~ s/^.*:://;
+    $self->{logger}->$method(@_);
+}
 
 1;
 __END__
---
+++ t/basic.t
@@ -0,0 +1,51 @@
+package MyLogger;
+use strict;
+use warnings;
+sub new { bless {warn => [], info => []} => $_[0] }
+sub info { push @{shift->{info}}, @_ }
+sub warn { push @{shift->{warn}}, @_ }
+
+package main;
+use strict;
+use warnings;
+use Log::Fibre;
+use Test::More;
+use Time::HiRes ();
+
+{
+    my $logger = MyLogger->new;
+    my $fibre = Log::Fibre->new($logger);
+    $fibre->warn("log1");
+    $fibre->info("log2");
+    is_deeply $logger->{warn}, [qw(log1)];
+    is_deeply $logger->{info}, [qw(log2)];
+}
+
+{
+    my $logger = MyLogger->new;
+    my $fibre = Log::Fibre->new($logger);
+    for (1 .. 6) {
+        $fibre->fibre(warn => "log1-$_", {max => 3});
+        $fibre->fibre(warn => "log2-$_", {max => 6});
+    }
+    is @{$logger->{warn}}, 3;
+    like $logger->{warn}[0], qr/log1-3/;
+    like $logger->{warn}[1], qr/log1-6/;
+    like $logger->{warn}[2], qr/log2-6/;
+}
+
+{
+    my $logger = MyLogger->new;
+    my $fibre = Log::Fibre->new($logger);
+    for (1 .. 7) { # 1.8 sec
+        $fibre->fibre(info => "log1-$_", {duration => 1});
+        $fibre->fibre(info => "log2-$_", {duration => 2});
+        Time::HiRes::sleep .3;
+    }
+    undef $fibre;
+
+    cmp_ok +(grep {/log1/} @{$logger->{info}}), '<=', 3;
+    cmp_ok +(grep {/log2/} @{$logger->{info}}), '<=', 2;
+}
+
+done_testing;
